9dc08bc55afc0b06bb2095894873fcb41e265c1d
imjesusc <iamjesus.ds@gmail.com>
2024-07-18 13:30:11 -0400

    feat: Update auth service with getEnvironments action

--- app/page.tsx
+++ app/page.tsx
@@ -3,20 +3,21 @@ import { getSession, signIn, signOut } from "./actions";
 
 export default async function Home() {
   const session = await getSession();
+  const isLoggedIn = !!session?.user?.id;
   return (
     <main>
       <h1>LinkedIn Posts</h1>
-      {!session?.user.id ? (
-        <form action={signIn}>
-          <button type="submit">Sign in</button>
-        </form>
-      ) : (
+      {isLoggedIn ? (
         <form action={signOut}>
           <button type="submit">Sign out</button>
         </form>
+      ) : (
+        <form action={signIn}>
+          <button type="submit">Sign in</button>
+        </form>
       )}
 
-      <AddPostForm />
+      {isLoggedIn ? <AddPostForm /> : null}
     </main>
   );
 }
--- services/auth-linkedin.service.ts
+++ services/auth-linkedin.service.ts
@@ -1,24 +1,25 @@
-"use server";
-
 import { userAdapter } from "@/adapters/user.adapter";
+import { getEnvironments } from "@/app/actions";
 import { ApiUser, User } from "@/models/user.model";
 
 export const getOAuthUrl = async (): Promise<string> => {
+  const env = await getEnvironments();
   const queryParams = new URLSearchParams();
-  queryParams.set("client_id", process.env.LINKEDIN_CLIENT_ID || "");
-  queryParams.set("redirect_uri", process.env.LINKEDIN_REDIRECT_URI || "");
+  queryParams.set("client_id", env.clientId);
+  queryParams.set("redirect_uri", env.redirectUri);
   queryParams.set("response_type", "code");
-  queryParams.set("state", process.env.LINKEDIN_STATE || "");
+  queryParams.set("state", env.linkedinState);
   queryParams.set("scope", "w_member_social,openid,profile,email");
 
   const response = await fetch(
-    `${process.env.LINKEDIN_API_URL}/oauth/v2/authorization?${queryParams.toString()}`,
+    `${env.apiUrl}/oauth/v2/authorization?${queryParams.toString()}`,
   );
 
   return response.url;
 };
 
 export async function getToken(code: string): Promise<{ accessToken: string }> {
+  const env = await getEnvironments();
   const OPTIONS = {
     method: "POST",
     headers: {
@@ -29,12 +30,12 @@ export async function getToken(code: string): Promise<{ accessToken: string }> {
   const queryParams = new URLSearchParams();
   queryParams.set("grant_type", "authorization_code");
   queryParams.set("code", code);
-  queryParams.set("client_id", process.env.LINKEDIN_CLIENT_ID || "");
-  queryParams.set("client_secret", process.env.LINKEDIN_CLIENT_SECRET || "");
-  queryParams.set("redirect_uri", process.env.LINKEDIN_REDIRECT_URI || "");
+  queryParams.set("client_id", env.clientId || "");
+  queryParams.set("client_secret", env.clientSecret || "");
+  queryParams.set("redirect_uri", env.redirectUri || "");
 
   const response = await fetch(
-    `${process.env.LINKEDIN_API_URL}/oauth/v2/accessToken?${queryParams.toString()}`,
+    `${env.apiUrl}/oauth/v2/accessToken?${queryParams.toString()}`,
     OPTIONS,
   );
   const data = await response.json();
@@ -45,6 +46,7 @@ export async function getToken(code: string): Promise<{ accessToken: string }> {
 }
 
 export async function getUser(accessToken: string): Promise<User> {
+  const env = await getEnvironments();
   const OPTIONS = {
     method: "GET",
     headers: {
@@ -53,10 +55,7 @@ export async function getUser(accessToken: string): Promise<User> {
     },
   };
 
-  const response = await fetch(
-    `${process.env.LINKEDIN_API_URL}/v2/userinfo`,
-    OPTIONS,
-  );
+  const response = await fetch(`${env.apiUrl}/v2/userinfo`, OPTIONS);
   const data: ApiUser = await response.json();
 
   return userAdapter(data);
