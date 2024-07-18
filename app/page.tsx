3e0414e193cb0e8c26b1bdcf077786af218a0437
imjesusc <iamjesus.ds@gmail.com>
2024-07-17 22:52:43 -0400

    feat: Add signIn and signOut features

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
-export default function Home() {
+import { AddPostForm } from "@/components/add-post-form";
+import { getSession, signIn, signOut } from "./actions";
+
+export default async function Home() {
+  const session = await getSession();
   return (
     <main>
       <h1>LinkedIn Posts</h1>
+      {!session?.user.id ? (
+        <form action={signIn}>
+          <button type="submit">Sign in</button>
+        </form>
+      ) : (
+        <form action={signOut}>
+          <button type="submit">Sign out</button>
+        </form>
+      )}
+
+      <AddPostForm />
     </main>
   );
 }
